547df86ddc001e2ba65c79fe42a7b0959500dfb0
Keshav Nagar <keshavnagar2k6@gmail.com>
2026-03-25 02:16:35 +0530

    Implement min and max search in array with optimized approach

---
+++ MinAndMax.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class MinAndMax {
+
+    public static int[] findMinMax(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int num : arr) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        return new int[]{min, max};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println("Array is empty");
+            return;
+        }
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] result = findMinMax(arr);
+
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+
+        sc.close();
+    }
+}
