57db61450ba288cc03db8f608fd68a6e0c900e40
Keshav Nagar <keshavnagar2k6@gmail.com>
2026-04-14 17:51:40 +0530

    Replace all the occurrence of an element with other in an array

---
+++ ReplaceOccurrence.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ReplaceOccurrence {
+    public static int[] replaceOccurrence(int []arr, int a, int b){
+
+         for(int i = 0; i < arr.length; i++){
+            if(arr[i] == a){
+                arr[i] = b;
+            }
+        }
+
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int [] arr = new int [n];
+        for(int i = 0; i<n ; i++){
+            arr[i]=sc.nextInt();
+        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int [] result = replaceOccurrence(arr, a,b);
+        for(int num : arr){
+            System.out.println(num);
+        }
+    }
+}
